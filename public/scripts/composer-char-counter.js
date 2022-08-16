d268444eefd61d6ea898594f302739465371fba8
Anthony Campbell <anthony-campbell@hotmail.com>
2022-08-15 19:24:58 -0700

    Update character count. Turn red if tweet is too long.

---
+++ public/scripts/composer-char-counter.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $('.new-tweet textarea').on('input', function(e){
+    const newVal = 140-$(this).val().length
+    let counter = $(this).parent().find('.counter')
+    if (newVal < 0){
+      counter.addClass('bad-counter');
+    }
+    newVal < 0 ? counter.addClass('red'): counter.removeClass('red')
+    counter.val(newVal);
+  });
+});
