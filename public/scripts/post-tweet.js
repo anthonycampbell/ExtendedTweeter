e25df6d29ce5ea2f07e656c0de4b5104ec9d7e4e
Anthony Campbell <anthony-campbell@hotmail.com>
2022-08-16 00:22:19 -0700

    Handle error in user input client side.

--- public/scripts/post-tweet.js
+++ public/scripts/post-tweet.js
@@ -2,6 +2,13 @@ $(document).ready(function(){
   $(".new-tweet form").submit(function(e){
     e.preventDefault();
     const data = $(this).serialize();
+    const text = $(this.text).val();
+    if (text === null || text.length === 0){
+      return alert("Tweet must contain content");
+    }
+    if (text.length > 140){
+      return alert("Tweet is too long");
+    }
     $.ajax('/tweets', { method: 'POST', data: data })
     .then(function (res) {
       console.log('Success', res);
