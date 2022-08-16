888658b1dc792572ba46a7bb42db595dceae456b
Anthony Campbell <anthony-campbell@hotmail.com>
2022-08-16 01:26:58 -0700

    Refactor error handling bad user input.

--- public/scripts/post-tweet.js
+++ public/scripts/post-tweet.js
@@ -6,15 +6,20 @@ const escape = function (str) {
 
 $(document).ready(function(){
   $(".new-tweet form").submit(function(e){
+    $(".error").hide();
     e.preventDefault();
     const text = $(this.text).val();
     $(this.text).val(escape(text));
     const data = $(this).serialize();
     if (text === null || text.length === 0){
-      return alert("Tweet must contain content");
+      $(".error").html("Tweet must contain content");
+      $(".error").slideDown();
+      return;
     }
     if (text.length > 140){
-      return alert("Tweet is too long");
+      $(".error").html("Tweet is too long");
+      $(".error").slideDown();
+      return;
     }
     $.ajax('/tweets', { method: 'POST', data: data })
     .then(function () {
