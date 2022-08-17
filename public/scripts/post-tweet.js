1231ab9ded68f054aa6e0ac4bdcd432977aedaf7
Anthony Campbell <anthony-campbell@hotmail.com>
2022-08-16 19:01:56 -0700

    Lint.

--- public/scripts/post-tweet.js
+++ public/scripts/post-tweet.js
@@ -44,11 +44,11 @@ $(document).ready(function() {
 });
 
 $(document).ready(function() {
-  $('nav button').click(function(){
+  $('nav button').click(function() {
     const $newTweet = $(".new-tweet");
-    if(!$newTweet.is(':visible')){
+    if (!$newTweet.is(':visible')) {
       $newTweet.slideDown();
-      const $form = $newTweet.children('form')
+      const $form = $newTweet.children('form');
       const $textArea = $form.children('textarea');
       $textArea.focus();
       return;
