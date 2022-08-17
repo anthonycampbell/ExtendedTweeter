fba7420f1dda6627bf95b1c7fb36e8d0557300fb
Anthony Campbell <anthony-campbell@hotmail.com>
2022-08-16 19:00:55 -0700

    Implement stretch toggle post new tweet form. Adjust style

--- public/scripts/post-tweet.js
+++ public/scripts/post-tweet.js
@@ -42,3 +42,17 @@ $(document).ready(function() {
       });
   });
 });
+
+$(document).ready(function() {
+  $('nav button').click(function(){
+    const $newTweet = $(".new-tweet");
+    if(!$newTweet.is(':visible')){
+      $newTweet.slideDown();
+      const $form = $newTweet.children('form')
+      const $textArea = $form.children('textarea');
+      $textArea.focus();
+      return;
+    }
+    $newTweet.slideUp();
+  });
+});
