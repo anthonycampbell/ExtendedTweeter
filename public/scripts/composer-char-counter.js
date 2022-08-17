da957c7fa35cb852523420857c2240287f145220
Anthony Campbell <anthony-campbell@hotmail.com>
2022-08-16 18:27:57 -0700

    Refactor write post nav button. Refactor counter for readability.

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,9 +2,6 @@ $(document).ready(function() {
   $('.new-tweet textarea').on('input', function() {
     const newVal = 140 - $(this).val().length;
     let counter = $(this).parent().find('.counter');
-    if (newVal < 0) {
-      counter.addClass('bad-counter');
-    }
     newVal < 0 ? counter.addClass('red') : counter.removeClass('red');
     counter.val(newVal);
   });
