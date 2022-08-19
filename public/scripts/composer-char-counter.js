aada3c171e32a96baf6ac53a156d3cbe49251e26
Anthony Campbell <anthony-campbell@hotmail.com>
2022-08-18 23:56:18 -0700

    Get counter navigating dom with jquery

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('.new-tweet textarea').on('input', function() {
     const newVal = 140 - $(this).val().length;
-    let counter = $(this).parent().find('.counter');
+    let counter = $(this).next().children('.counter');
     newVal < 0 ? counter.addClass('red') : counter.removeClass('red');
     counter.val(newVal);
   });
